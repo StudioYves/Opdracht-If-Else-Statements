9f54dc54baefd2f9828d8663966fbc44f313aa83
Ivo Kersten <82444976+StudioYves@users.noreply.github.com>
2021-05-31 17:41:06 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const age = 16;
+const isFemale = true;
+const driverStatus = 'bob';
+
+if (age > 17){
+    console.log("Welkom. Je mag in deze kroeg naar binnen!")
+}else{
+    console.log("Je bent helaas nog te jong. Helaas moet ik je verzoeken om buiten te blijven.")
+};
+
+if (isFemale == true){
+    console.log("Als vrouw ben je van harte welkom vanavond!")
+}else{
+    console.log("Pech gehad, het is vanavond Ladies Only!")
+};
+
+if (driverStatus == 'bob'){
+    console.log('Keurig, je hebt geen alcohol gedronken en mag zelf naar huis rijden.')
+} else{
+    console.log('Je hebt teveel alochol gedronken. Het is niet toegestaan om zelf naar huis te rijden.')
+};
+
